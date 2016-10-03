fea93ba3c09cb3e08879edaeb280cf323f604078
dapeng126 <zhangdapeng89@126.com>
2016-10-03 19:23:27 +0800

    添加 Message的其他使用方式

--- app/src/main/java/com/gowhich/androidhandlerandmessage/MainActivity.java
+++ app/src/main/java/com/gowhich/androidhandlerandmessage/MainActivity.java
@@ -27,22 +27,22 @@ public class MainActivity extends AppCompatActivity {
     private String imagePath = "http://d.hiphotos.baidu.com/image/pic/item/d0c8a786c9177f3e3ca2960c72cf3bc79f3d5618.jpg";
     private final int IS_FINISH = 1;
     ProgressDialog progressDialog = null;
-    private Handler handler = new Handler(){
+    private Handler handler = new Handler() {
 
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
 
-            byte[] data = (byte[] )msg.obj;
+            byte[] data = (byte[]) msg.obj;
             Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
             imageView.setImageBitmap(bitmap);
-            if(msg.what == IS_FINISH){
+            if (msg.what == IS_FINISH) {
                 progressDialog.dismiss();
             }
         }
     };
 
-    public class MyThread implements Runnable{
+    public class MyThread implements Runnable {
 
         @Override
         public void run() {
@@ -51,17 +51,56 @@ public class MainActivity extends AppCompatActivity {
             HttpResponse httpResponse = null;
 //            InputStream inputStream = null;
 
-            try{
+            try {
                 httpResponse = httpClient.execute(httpGet);
-                if(httpResponse.getStatusLine().getStatusCode() == 200){
+                if (httpResponse.getStatusLine().getStatusCode() == 200) {
 //                    inputStream = httpResponse.getEntity().getContent();
                     byte[] data = EntityUtils.toByteArray(httpResponse.getEntity());
                     Message message = Message.obtain();
                     message.obj = data;
                     message.what = IS_FINISH;
                     handler.sendMessage(message);
+
+                    //第一种方式
+//                    Message message1 = Message.obtain();
+//                    message1.obj = data;
+//                    message1.what = IS_FINISH;
+//                    message1.arg1 = "";
+//                    message1.arg2 = "":
+//                    handler.sendMessage(message1);
+
+                    //第二种方式
+//                    Message message1 = Message.obtain(handler);
+//                    message1.obj = data;
+//                    message1.what = IS_FINISH;
+//                    message1.arg1 = "";
+//                    message1.arg2 = "":
+//                    message1.sendToTarget();
+
+                    //第三种方式
+//                    Message message1 = Message.obtain(handler, 33);
+//                    message1.obj = data;
+//                    message1.what = IS_FINISH;
+//                    message1.arg1 = "";
+//                    message1.arg2 = "":
+//                    message1.sendToTarget();
+                    //第四种方式
+//                    Message message1 = Message.obtain(handler, 2, data);
+//                    message1.sendToTarget();
+
+                    //第五种方式
+//                    Message message1 = Message.obtain(handler, 9, 1, 2, data);
+//                    message1.sendToTarget();
+
+                    //第六种方式
+//                    Message message1 = Message.obtain(handler, 9, 1, 2, data);
+//                    Bundle bundle = new Bundle();
+//                    bundle.putStringArray("name",new String[]{"wang","li","zhao"});
+//                    message1.setData(bundle);
+//                    message1.sendToTarget();
+
                 }
-            }catch(Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
